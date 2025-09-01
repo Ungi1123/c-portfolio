53e4f3540d5f9efc02e04b7bb2271242fc0bb0d7
Ungi1123 <heunre11@gmail.com>
2025-09-01 17:45:21 +0900

    Modify : Contents

--- Prediction_Age/Prediction_Age_Ver2.c
+++ Prediction_Age/Prediction_Age_Ver2.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 
-int main(void){
+int main(){
+
     int birth;
     int year;
 
-    scanf("%d %d", &birth, &year);
-    printf("%d년에 나는 %d살이야!",year, year - birth);
+    scanf("%d %d", &birth, & year);
+
+    int age = year - birth;
+
+    printf("%d년에 나는 %d살이야!", year, age);
 
     return 0;
 
